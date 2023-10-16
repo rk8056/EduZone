0961d2a30d797be62061d303fa05d87fe4cd4126
rk8056 <rk805689@gmail.com>
2023-10-16 17:51:54 +0530

    changes in home

--- EduZone/main/js/script.js
+++ EduZone/main/js/script.js
@@ -2,22 +2,16 @@ const header=document.querySelector(".header");
 window.addEventListener("scroll",()=>{
     if(window.scrollY>1){
         header.style.backgroundColor="#6500d8";
+        header.classList.add("active");
     }else{
         header.style.backgroundColor="transparent";
+        header.classList.remove("active");
     }
 });
 
-const logoSmall=document.querySelector(".logo-small");
-const logoLight=document.querySelector(".logo-light");
-const logoDark=document.querySelector(".logo-dark");
-window.addEventListener("scroll",()=>{
-    if(window.scrollY>1){
-        logoLight.style.opacity="0";
-        logoDark.style.display="inline";
-    }else{
-        logoLight.style.opacity="1";
-        logoDark.style.display="none";
-    }
-});
+
+
+
+
 
 
